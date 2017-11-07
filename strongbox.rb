b4aaf295a26474e465333c0f16a48f5eb580fdd0
Stian Kristoffersen <stian.kristoffersen@schibsted.com>
2017-11-07 17:49:35 +0100

    Fix hash for version 0.2.4

--- strongbox.rb
+++ strongbox.rb
@@ -3,7 +3,7 @@ class Strongbox < Formula
   homepage "https://github.com/schibsted/strongbox"
   url "https://dl.bintray.com/strongbox/strongbox-generic/strongbox-cli-0.2.4.tar.gz"
   version "0.2.4"
-  sha256 "4965a4ca18a33a9987a25e167f7ecf0957b3fbeef16ff9c69fd2a5f37691992f"
+  sha256 "f33ac88d03afd054a537720971c5ec874c1e8970f877e565d1ce77c1d173183f"
 
   def install
     bin.install Dir["bin/*"]

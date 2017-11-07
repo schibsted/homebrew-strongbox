79b444a120059808a9d74d2e4183cf4709ee0314
Stian Kristoffersen <stian.kristoffersen@schibsted.com>
2017-11-07 17:40:47 +0100

    Upgrade to version 0.2.4

--- strongbox.rb
+++ strongbox.rb
@@ -1,9 +1,9 @@
 class Strongbox < Formula
   desc "Strongbox"
   homepage "https://github.com/schibsted/strongbox"
-  url "https://dl.bintray.com/strongbox/strongbox-generic/strongbox-cli-0.2.3.tar.gz"
+  url "https://dl.bintray.com/strongbox/strongbox-generic/strongbox-cli-0.2.4.tar.gz"
   version "0.2.3"
-  sha256 "e736adf43d530d9391f5375f034c113a27c662636215dd3ef368ae30dff32afb"
+  sha256 "4965a4ca18a33a9987a25e167f7ecf0957b3fbeef16ff9c69fd2a5f37691992f"
 
   def install
     bin.install Dir["bin/*"]

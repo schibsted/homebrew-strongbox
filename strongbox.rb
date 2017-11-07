374c1b2ca569286825432957033baba1a20fa64b
Stian Kristoffersen <stian.kristoffersen@schibsted.com>
2017-11-07 17:44:38 +0100

    Fix version number

--- strongbox.rb
+++ strongbox.rb
@@ -2,7 +2,7 @@ class Strongbox < Formula
   desc "Strongbox"
   homepage "https://github.com/schibsted/strongbox"
   url "https://dl.bintray.com/strongbox/strongbox-generic/strongbox-cli-0.2.4.tar.gz"
-  version "0.2.3"
+  version "0.2.4"
   sha256 "4965a4ca18a33a9987a25e167f7ecf0957b3fbeef16ff9c69fd2a5f37691992f"
 
   def install

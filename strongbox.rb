8fb5881f224b2b90e58b5ec9edb033925fdd1d16
Travis CI User <travis@example.org>
2017-12-04 17:33:59 +0000

    Upgraded to version 0.2.13

--- strongbox.rb
+++ strongbox.rb
@@ -1,9 +1,9 @@
 class Strongbox < Formula
   desc "Strongbox"
   homepage "https://github.com/schibsted/strongbox"
-  url "https://dl.bintray.com/strongbox/strongbox-generic/strongbox-cli-0.2.7.tar.gz"
-  version "0.2.7"
-  sha256 "86e7602bc74039e23785ac26d2a9371e31746837f0142d6e8d5763eb2cbb0d7c"
+  url "https://dl.bintray.com/strongbox/strongbox-generic/strongbox-cli-0.2.13.tar.gz"
+  version "0.2.13"
+  sha256 "23a87fb00b16cf8f8543cc387fe40f0c13209c97b705dbc0f9d684204f551f4c"
 
   def install
     bin.install Dir["bin/*"]
